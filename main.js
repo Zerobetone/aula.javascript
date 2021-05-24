b71d5e2ce218b35d00f9709eeb2c8790a777f39f
Zerobetone <84295963+Zerobetone@users.noreply.github.com>
2021-05-23 22:29:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por Clicar";
+   // console.log( document.getElementById("agradecimento"));
+}
+function redirecionar(){
+    window.open("https://github.com/Zerobetone");
+}
+function trocar(){
+    document.getElementById("mouseover").innerHTML = "Obrigado por passar o mouse";
+    
+}
